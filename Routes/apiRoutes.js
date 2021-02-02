ce238d06487fb17e724c6e35aaf7078cce79f17c
Wendy Fgigueroa <camomila444@gmail>
2021-02-01 21:54:54 -0500

    trying to deploy heroku

--- Routes/apiRoutes.js
+++ Routes/apiRoutes.js
@@ -3,12 +3,6 @@ const noteData = require("../db/db.json");
 const uniqid = require('uniqid');
 
 
- 
-console.log(uniqid()); // -> 4n5pxq24kpiob12og9
-console.log(uniqid(), uniqid()); // -> 4n5pxq24kriob12ogd, 4n5pxq24ksiob12ogl
-
-
-
 
 module.exports = function(app) {
 
