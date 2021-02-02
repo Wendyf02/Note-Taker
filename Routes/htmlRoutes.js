b5bfd1cd4a055c6ceb81da1a92be0db29ac54d1c
Wendy Fgigueroa <camomila444@gmail>
2021-02-01 19:34:09 -0500

    trying to deploy to herku

--- Routes/htmlRoutes.js
+++ Routes/htmlRoutes.js
@@ -8,6 +8,9 @@ module.exports = function(app) {
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 
 
 //HTML Route to go to index page
